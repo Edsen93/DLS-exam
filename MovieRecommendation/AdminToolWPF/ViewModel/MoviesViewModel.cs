7a1f84831b438868c473d3449c633f2e14355fa8
DJH <dj650hansen@gmail.com>
2019-05-14 10:33:52 +0200

    Remove green text

--- MovieRecommendation/AdminToolWPF/ViewModel/MoviesViewModel.cs
+++ MovieRecommendation/AdminToolWPF/ViewModel/MoviesViewModel.cs
@@ -89,70 +89,7 @@ namespace AdminToolWPF.ViewModel
                 RaisePropertyChanged("SelectedMovie");
             }
         }
-
-        //private Movie _movie;
-        //private ObservableCollection<GenreViewModel> _genreViewModelList = new ObservableCollection<GenreViewModel>();
-
-
-        //private string _title = "";
-        //public string Title
-        //{
-        //    get { return _title; }
-        //    set
-        //    {
-        //        _title = value;
-        //        RaisePropertyChanged("Title");
-        //    }
-        //}
-
-
-        //private int _releaseYear = 1800;
-        //public int ReleaseYear
-        //{
-        //    get { return _releaseYear; }
-        //    set
-        //    {
-        //        _releaseYear = value;
-        //        RaisePropertyChanged("ReleaseYear");
-        //    }
-        //}
-
-
-        //public ObservableCollection<GenreViewModel> GenreViewModelList
-        //{
-        //    get { return _genreViewModelList; }
-        //    set
-        //    {
-        //        _genreViewModelList = value;
-        //        RaisePropertyChanged("GenreViewModelList");
-        //    }
-        //}
-
-
-
-
-
-        //private ObservableCollection<Movie> _movieViewModelList = new ObservableCollection<Movie>();
-        //public ObservableCollection<Movie> MovieViewModelList
-        //{
-        //    get { return _movieViewModelList; }
-        //    set
-        //    {
-        //        _movieViewModelList = value;
-        //        RaisePropertyChanged("MovieViewModelList");
-        //    }
-        //}
-
-        //private bool _isNewMovie = false;
-        //public bool IsNewMovie
-        //{
-        //    get { return _isNewMovie; }
-        //    set
-        //    {
-        //        _isNewMovie = value;
-        //        RaisePropertyChanged("IsNewMovie");
-        //    }
-        //}
+        
 
         private CollectionViewSource moviesCollection;
 
@@ -200,66 +137,8 @@ namespace AdminToolWPF.ViewModel
 
             GetMovies();
             
-            //GenreStuff();
-            //RaisePropertyChanged("GenreViewModel");
-            //RaisePropertyChanged("GenreViewModelList");
-            //RaisePropertyChanged("Title");
         }
 
-
-        private void GenreStuff()
-        {
-
-            //GenreViewModelList.Clear();
-
-            //List<Genre> GenreList = new List<Genre>()
-            //{
-            //    new Genre(){ GenreId = 1, GenreText = "Action" },
-            //    new Genre(){ GenreId = 2, GenreText = "Adventure" },
-            //    new Genre(){ GenreId = 3, GenreText = "Animation" },
-            //    new Genre(){ GenreId = 4, GenreText = "Children" },
-            //    new Genre(){ GenreId = 5, GenreText = "Comedy" },
-            //    new Genre(){ GenreId = 6, GenreText = "Crime" },
-            //    new Genre(){ GenreId = 7, GenreText = "Documentary" },
-            //    new Genre(){ GenreId = 8, GenreText = "Drama" },
-            //    new Genre(){ GenreId = 9, GenreText = "Fantasy" },
-            //    new Genre(){ GenreId = 10, GenreText = "Film-Noir" },
-            //    new Genre(){ GenreId = 11, GenreText = "Horror" },
-            //    new Genre(){ GenreId = 12, GenreText = "Musical" },
-            //    new Genre(){ GenreId = 13, GenreText = "Mystery" },
-            //    new Genre(){ GenreId = 14, GenreText = "Romance" },
-            //    new Genre(){ GenreId = 15, GenreText = "Sci-Fi" },
-            //    new Genre(){ GenreId = 16, GenreText = "Thriller" },
-            //    new Genre(){ GenreId = 17, GenreText = "War" },
-            //    new Genre(){ GenreId = 18, GenreText = "Western" }
-            //};
-
-            //foreach (Genre gen in GenreList)
-            //    GenreViewModelList.Add(new GenreViewModel(gen));
-
-
-            //if (model == null || model.MovieId < 1)
-            //{
-            //    IsNewMovie = true;
-            //    _movie = new Movie();
-            //}
-            //else
-            //{
-            //    IsNewMovie = false;
-            //    _movie = model;
-            //    ReleaseYear = _movie.ReleaseYear;
-            //    Title = _movie.Title;
-
-            //    foreach (Genre genre in _movie.GenreList)
-            //    {
-            //        GenreViewModel found = GenreViewModelList.FirstOrDefault(x => x.GenreId == genre.GenreId);
-            //        if (found != null)
-            //            found.IsSelected = true;
-            //    }
-
-            //}
-
-        }
         
 
         private void GetMovies(bool loadMovies = false)
