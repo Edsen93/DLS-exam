bf95f6fd88ae1d3b6897a255b7c974fc34484eb5
DJH <dj650hansen@gmail.com>
2019-05-15 23:06:02 +0200

    added GET request handler

--- AdminSolution/AdminToolWPF/ViewModel/TestMethodsViewModel.cs
+++ AdminSolution/AdminToolWPF/ViewModel/TestMethodsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using GalaSoft.MvvmLight;
+﻿using AdminToolWPF.Helper_Classes;
+using GalaSoft.MvvmLight;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -72,26 +73,19 @@ namespace AdminToolWPF.ViewModel
 
         public IRelayCommand GetAllMoviesCommand => new RelayCommand(() =>
         {
-            SetDemo();
-
+            ConsoleText = RequestHandler.Get($"{ConnetionSettings.PostgresMoviesAddress}/api/movies");
         });
 
         public IRelayCommand GetMovieByIDCommand => new RelayCommand(() =>
         {
-
-            SetDemo();
+            ConsoleText = RequestHandler.Get($"{ConnetionSettings.PostgresMoviesAddress}/api/movies/{MovieID}");
         }, () => !String.IsNullOrWhiteSpace(MovieID));
 
 
    
-        //public IRelayCommand<string> GetMovieByTitleCommand => new RelayCommand<string>((string movieTitle) =>
-        //{
-        //    SetDemo();
-        //}, (movieTitle) => movieTitle != null);
-
         public IRelayCommand GetMovieByTitleCommand => new RelayCommand(() =>
         {
-            SetDemo();
+            ConsoleText = RequestHandler.Get($"{ConnetionSettings.PostgresMoviesAddress}/api/movies/{MovieTitle}");
         }, () => !String.IsNullOrWhiteSpace(MovieTitle));
 
 
@@ -99,30 +93,15 @@ namespace AdminToolWPF.ViewModel
 
         public IRelayCommand GetAllUsersCommand => new RelayCommand(() =>
         {
-
-            SetDemo();
+            ConsoleText = RequestHandler.Get($"{ConnetionSettings.PostgresUserAddress}/api/users");
         });
 
 
         public IRelayCommand GetUserByIDCommand => new RelayCommand(() =>
         {
-            SetDemo();
+            ConsoleText = RequestHandler.Get($"{ConnetionSettings.PostgresUserAddress}/api/users/{UserID}");
         },() => !String.IsNullOrWhiteSpace(UserID));
 
-
-
-        private void SetDemo()
-        {
-            ConsoleText = @"
-Lorem ipsum dolor sit amet, consectetur adipiscing elit.Morbi rutrum magna est, ut placerat nisl posuere quis. Nam sed odio in tellus pretium molestie.Sed eleifend risus cursus dolor auctor, et sodales quam luctus.Maecenas congue varius hendrerit. Aliquam sem dui, rhoncus vitae nunc nec, ultricies iaculis nulla. Curabitur et nibh enim. Nam id pulvinar lorem, viverra lacinia eros. Pellentesque lacinia dolor dolor, ut commodo mi condimentum vitae. Pellentesque at orci dolor. Integer ut malesuada dui, efficitur posuere dui. Sed iaculis elit est, ut placerat est vulputate sed. Integer nec condimentum elit, quis blandit eros.
-
-Vivamus posuere cursus ex, in elementum leo malesuada congue. Fusce pellentesque, purus nec dignissim faucibus, mi leo ultricies magna, ut finibus metus leo ac felis.Maecenas id quam a turpis gravida tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nam gravida, tortor nec auctor pellentesque, orci augue semper mi, ac vehicula nunc ante eget ante.Proin sagittis lectus augue, eget convallis libero ullamcorper in. Sed imperdiet rhoncus egestas. Nulla sit amet hendrerit purus.Nam tristique nulla nec urna feugiat finibus.Suspendisse at leo eu ligula lobortis congue.Phasellus interdum leo consequat faucibus efficitur. In justo tellus, viverra eu felis eget, bibendum porta augue. Mauris blandit finibus ligula, eu volutpat libero eleifend consectetur. Integer non scelerisque diam, a ultricies lectus.
-
-Integer quis auctor mauris. Nulla pulvinar interdum sem vel tempus. Phasellus mattis risus vel iaculis iaculis. In ac fringilla nibh. Pellentesque quam tortor, mattis eu semper at, placerat eu erat. Maecenas condimentum hendrerit quam, eget tristique orci pulvinar malesuada. Donec iaculis tellus vitae mauris malesuada efficitur.Quisque faucibus tincidunt lacus, sed convallis eros.
-
-Nunc arcu libero, dapibus vitae rhoncus eget, molestie eu purus. Ut eget euismod ante, nec sodales mauris. Aenean sed euismod risus. Nunc magna enim, laoreet et elementum blandit, volutpat quis leo. Fusce at sagittis tellus. Praesent ac euismod lorem, quis semper diam. Vivamus eget urna ac nisi facilisis aliquam.Morbi quam dui, feugiat quis porttitor in, posuere nec ipsum.Nulla facilisis suscipit nulla nec scelerisque. Integer eros dolor, suscipit non malesuada quis, porta sit amet lorem.Nulla facilisi. Nam id arcu cursus, rutrum nisi vitae, molestie nisl.
-
-Proin elementum semper ipsum, sed imperdiet enim mollis sed. Nulla vel mauris dignissim, auctor lectus vel, aliquam tortor.Aliquam libero tellus, accumsan sed massa pretium, ultricies laoreet nunc. Morbi non dictum dui, sit amet faucibus purus.Sed in metus nec neque pellentesque condimentum.Duis pulvinar nec nulla vitae finibus. Mauris ornare quis diam in accumsan.Phasellus sit amet mauris aliquam elit euismod tempor sed eu arcu.";
-        }
+        
     }
 }
