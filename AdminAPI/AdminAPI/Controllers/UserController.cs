d603be5b4f5f9bfaba637b1dd84da3807e4bf25a
basdamgaard <sebastiandamgaard@gmail.com>
2019-05-28 13:58:15 +0200

    AdminAPI with complete implementation of User

---
+++ AdminAPI/AdminAPI/Controllers/UserController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminAPI.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        HttpClient client;
+
+        public UserController()
+        {
+            client = new HttpClient();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<string>> GetAllUsers()
+        {
+            var url = "https://dlsusermicroservice.azurewebsites.net/api/users";
+            string content = await client.GetStringAsync(url);
+            return content;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> GetUser(int id)
+        {
+            var url = string.Format("https://dlsusermicroservice.azurewebsites.net/api/users/{0}", id.ToString());
+            string content = await client.GetStringAsync(url);
+            return content;
+        }
+
+        [HttpGet("{username}/{password}")]
+        public async Task<ActionResult<string>> Login(string username, string password)
+        {
+            var url = string.Format("https://dlsusermicroservice.azurewebsites.net/api/users/{0}/{1}", username, password);
+            string content = await client.GetStringAsync(url);
+            return content;
+        }
+
+        [HttpPost]
+        public async Task CreateUser([FromBody]string user)
+        {
+            var url = "https://dlsusermicroservice.azurewebsites.net/api/users/";
+            var content = await client.PostAsJsonAsync<string>(url, user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task UpdateUser(int id, [FromBody]string user)
+        {
+            var url = string.Format("https://dlsusermicroservice.azurewebsites.net/api/users/{0}", id.ToString());
+            var content = await client.PutAsJsonAsync<string>(url, user);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task DeleteUser(int id)
+        {
+            var url = string.Format("https://dlsusermicroservice.azurewebsites.net/api/users/{0}", id.ToString());
+            var content = await client.DeleteAsync(url);
+        }
+    }
+}
