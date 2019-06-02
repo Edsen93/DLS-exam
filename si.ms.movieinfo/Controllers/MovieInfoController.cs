62474d0f4fb4871efe94af4da5c243158ca7c9b5
Edsen93 <emiledsen93@gmail.com>
2019-06-02 12:57:32 +0200

    added validation on crete movie

--- si.ms.movieinfo/Controllers/MovieInfoController.cs
+++ si.ms.movieinfo/Controllers/MovieInfoController.cs
@@ -13,37 +13,105 @@ namespace si.ms.movieinfo.Controllers
     public class MovieInfoController : ControllerBase
     {
         [HttpGet]
-        public List<Movie> Get()
+        public ActionResult<List<Movie>> Get()
         {
-            MoviePersistence mp = new MoviePersistence();
-            return mp.ReturnAllMovies();
+            try
+            {
+                MoviePersistence mp = new MoviePersistence();
+                var movieList = mp.ReturnAllMovies();
+                if (movieList.Count == 0)
+                {
+                    return Conflict("There is no movies in the database");
+                }
+                else
+                {
+                    return mp.ReturnAllMovies();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Conflict(ex.Message);
+
+            }
         }
 
-
+        //search after movie title /api/movieinfo/batman
         [HttpGet("title/{title}")]
-        public List<Movie> Get(String title)
+        public ActionResult<List<Movie>> Get(String title)
         {
-            MoviePersistence mp = new MoviePersistence();
-            return mp.SearchForMovie(title);
+            try
+            {
+                MoviePersistence mp = new MoviePersistence();
+                var movielist = mp.SearchForMovie(title);
+                if (movielist.Count <= 0)
+                {
+                    return Conflict("No movie was found");
+                }
+                return movielist;
+            }
+            catch (Exception ex)
+            {
+
+                return Conflict(ex.Message);
+            }
         }
 
         // GET: api/Default/5
         [HttpGet("{id}", Name = "Get")]
-        public Movie Get(int id)
+        public ActionResult<Movie>  Get(int id)
         {
-            MoviePersistence mp = new MoviePersistence();
+            try
+            {
+                MoviePersistence mp = new MoviePersistence();
+                Movie foundMovie = mp.findOneMovie(id);
 
-            return mp.findOneMovie(id);
+                if (String.IsNullOrEmpty(foundMovie.title))
+                {
+                    var errorstr = String.Format("A movie with id {0} does not exits in the database", id);
+                    return Conflict(errorstr);
+                }
+                else
+                {
+                    return foundMovie;
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return Conflict(ex.Message);
+                
+            }
         }
 
 
         // POST: api/Default
         [HttpPost]
-        public Movie Post([FromBody]Movie value)
+        public ActionResult<Movie> Post([FromBody]Movie value)
         {
-            MoviePersistence mp = new MoviePersistence();
+            try
+            {
+                Movie m = value;
+                MoviePersistence mp = new MoviePersistence();
+                var currentyear = System.DateTime.Now.Year;
+                if (m.releaseYear>= 1950 && m.releaseYear < currentyear)
+                {
+                    return mp.SaveMovie(m);
+                }
+                else
+                {
+                    var errorstr = String.Format("{0} Is not a valid year", m.releaseYear);
+                    return Conflict(errorstr);
+                }
 
-            return mp.SaveMovie(value);
+            }
+            catch (Exception ex)
+            {
+                return Conflict(ex.Message);
+                
+            }          
+          
         }
 
         // PUT: api/Default/5
